4a2861295b36e34deeda353ce4579c3380a85cab
geb2701 <86382687+geb2701@users.noreply.github.com>
2023-11-17 00:21:02 -0300

    arreglos

---
+++ proyecto santoro/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using proyecto_santoro.Models;
+
+namespace proyecto_santoro.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ProyectoContext _context;
+
+        public AccountController(ProyectoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            // Devolver la vista 'Register.cshtml' desde la carpeta 'Views/Home'
+            return View("~/Views/Home/Register.cshtml");
+        }
+
+        [HttpGet]
+        public IActionResult GetData()
+        {
+            // Devolver la vista 'Register.cshtml' desde la carpeta 'Views/Home'
+            return View("~/Views/Home/Register.cshtml");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(string userName, string password)
+        {
+
+            if (userName != null) {
+                return BadRequest("esta mal");
+            }
+
+            var usuarioNuevo = new Table1()
+            {
+                Usuario = userName,
+                Contraseña = password
+            };
+
+            _context.Add(usuarioNuevo);
+            await _context.SaveChangesAsync();
+
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ proyecto santoro/Views/Table1/Index1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace proyecto_santoro.Views.Table1
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
